559a49ade326dbf76c916b9384519ea6e9127aab
Zachary Elbring <elbringzach@gmail.com>
2024-03-11 15:54:16 -0500

    Added schemas folder and defined typedefs.

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const typeDefs = `
+input Bookinput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String!
+    image: String
+    link: String
+}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Auth {
+    token: ID!
+    user: User!
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(userId: ID!, bookId: String!): User
+}
+`
+
+module.exports = typeDefs;
