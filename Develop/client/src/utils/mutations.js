9ab870dc55a2cc373e291fa68eb1e6b9075b1083
Zachary Elbring <elbringzach@gmail.com>
2024-03-11 16:40:24 -0500

    Created mutations for client.

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,52 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+        }
+    }
+}`
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            email
+        }
+    }
+}`
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($userId: ID!, $bookId: String!) {
+    removeBook(userId: $userId, bookId: $bookId) {
+        savedBooks {
+            bookId
+            authors
+            description
+            image
+            title
+            link
+        }
+    }
+}`
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+        savedBooks {
+            bookId
+            authors
+            description
+            image
+            title
+            link
+        }
+    }
+}`
