167b075f6efd3fc42fdcc22458f6e67a9d5c40f5
Zachary Elbring <elbringzach@gmail.com>
2024-03-11 16:49:36 -0500

    created apollo provider in app.jsx for handling requests.

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+const auth = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+const httpLink = createHttpLink({uri: '/graphql'});
+const client = new ApolloClient({
+  link: auth.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ApolloProvider />
     </>
   );
 }
