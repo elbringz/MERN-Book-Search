8f8bd1de20a317e443afb1467a7691226e6ad386
Zachary Elbring <elbringzach@gmail.com>
2024-03-11 16:30:42 -0500

    Added client query to utils folder.

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_GET_ME = gql`
+query me ($_id: ID!) {
+    me(userId: $_id) {
+        _id
+        email
+        username
+        savedBooks {
+            authors
+            bookId
+            description
+            image
+            link
+            title
+        }
+    }
+}`
